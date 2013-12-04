e6d0b99cc84ab7ccba5620ce3bb6712b91817d57
BerlinSun <bolinsun@outlook.com>
2013-12-04 14:02:08 +0800

    Remove stdafx dependence.

--- NewStandardTest/NewStandardTest.cpp
+++ NewStandardTest/NewStandardTest.cpp
@@ -1,21 +1,19 @@
 // NewStandardTest.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <vector>
 #include <iostream>
 using std::vector;
 using std::cout;
 using std::endl;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	vector<int> nums = { 1, 3, 5, 7, 9 };
 	for (auto i : nums)
 	{
 		cout << i << endl;
 	}
-	system("PAUSE");
 	return 0;
 }
 
